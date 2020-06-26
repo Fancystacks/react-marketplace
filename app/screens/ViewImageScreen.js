0e91fec9a98b1ab68ce7cb167d68cd55793ff9f2
Leah Migacz <lmigacz@msudenver.edu>
2020-06-26 02:34:40 -0600

    view item page 🛒

---
+++ app/screens/ViewImageScreen.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { Image, StyleSheet, View } from 'react-native';
+
+function ViewImageScreen(props) {
+    return (
+        <View style={styles.container}> 
+        <View style={styles.closeIcon}></View>
+        <View style={styles.deleteIcon}></View>
+            <Image
+            style={styles.image} 
+            source={require('../assets/chair.jpg')} />
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+ 
+    closeIcon: {
+        width: 50,
+        backgroundColor: 'black',
+        position: 'absolute',
+        height: 50,
+        top: 40,
+        left: 25
+    },
+    deleteIcon: {
+        width: 50,
+        backgroundColor: 'red',
+        position: 'absolute',
+        height: 50,
+        top: 40,
+        right: 25
+    },
+    image: {
+     width: '100%',
+     opacity: .8,
+     height: '100%'
+ }
+});
+
+export default ViewImageScreen;
