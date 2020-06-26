0ff3d44dc25d75b0cd2bc0b5fea9e7a572cd386e
Leah Migacz <lmigacz@msudenver.edu>
2020-06-26 00:20:29 -0600

    added wallpapers 🛒

--- App.js
+++ App.js
@@ -1,21 +1,11 @@
 import React from 'react';
-import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import { View } from 'react-native';
+import WelcomeScreen from "./app/screens/WelcomeScreen";
 
 export default function App() {
-  let x = 1;
-  console.log("Hello Moto");
+  
   return (
-    <SafeAreaView style={styles.container}>
-      <Text>Hello Nain and Budgies!</Text>
-    </SafeAreaView>
+   <WelcomeScreen/>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'dodgerblue',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
