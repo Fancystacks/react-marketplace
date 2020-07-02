bc298a41ddfd70e5c8a134ac97062c66c2a85d3a
Leah Migacz <lmigacz@msudenver.edu>
2020-07-01 21:52:42 -0600

    Yup Validation 🛒

--- app/screens/LoginScreen.js
+++ app/screens/LoginScreen.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import { StyleSheet, Image } from "react-native";
+import { Formik } from 'formik';
+import * as Yup from 'yup';
 
 import Screen from '../components/Screen';
 import AppTextInput from '../components/AppTextInput';
 import AppButton from '../components/AppButton';
-import { Formik } from 'formik';
+import AppText from '../components/AppText/AppText';
+
+const validationSchema = Yup.object().shape({
+    email: Yup.string().required().email().label("Email"),
+    password: Yup.string().required().min(6).label("Password")
+});
 
 function LoginScreen(props) {
 
@@ -12,12 +19,14 @@ function LoginScreen(props) {
         <Screen style={styles.container}>
             <Image
                 style={styles.logo}
-                source={require("../assets/trolly.png")} />
+                source={require("../assets/trolly.png")} 
+                />
             <Formik
                 initialValues={{ email: '', password: '' }}
-                onSubmit={values => console.log(values)} >
+                onSubmit={values => console.log(values)}
+                validationSchema={validationSchema} >
 
-                {({ handleChange, handleSubmit }) => (
+                {({ handleChange, handleSubmit, errors}) => (
                     <>
                         <AppTextInput
                             autoCapitalize="none"
@@ -28,6 +37,7 @@ function LoginScreen(props) {
                             placeholder="Email"
                             textContentType="emailAddress" // iOS fill from keychain
                         />
+                        <AppText style={{ color: 'red' }}>{errors.email}</AppText>
                         <AppTextInput
                             autoCapitalize="none"
                             autoCorrect={false}
@@ -37,6 +47,7 @@ function LoginScreen(props) {
                             secureTextEntry
                             textContentType="password"
                         />
+                        <AppText style={{ color: 'red' }}>{errors.password}</AppText>
                         <AppButton title="LOG IN" onPress={handleSubmit} />
                     </>
                 )}
