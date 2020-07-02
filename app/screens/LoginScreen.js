c5590b4cbe481fab2207695def48ede684ae8b47
Leah Migacz <lmigacz@msudenver.edu>
2020-07-01 21:33:07 -0600

    Formik Library 🛒

--- app/screens/LoginScreen.js
+++ app/screens/LoginScreen.js
@@ -1,38 +1,47 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { StyleSheet, Image } from "react-native";
 
 import Screen from '../components/Screen';
 import AppTextInput from '../components/AppTextInput';
 import AppButton from '../components/AppButton';
+import { Formik } from 'formik';
 
 function LoginScreen(props) {
-    const [email, setEmail] = useState();
-    const [password, setPassword] = useState();
 
     return (
         <Screen style={styles.container}>
-            <Image 
-            style={styles.logo}
-            source ={require("../assets/trolly.png")} />
-            <AppTextInput
-            autoCapitalize="none"
-            autoCorrect={false}
-            icon="email-outline"
-            keyboardType="email-address"
-            onChangeText={text => setEmail(text)}
-            placeholder="Email"
-            textContentType="emailAddress" // iOS fill from keychain
-            />
-            <AppTextInput
-            autoCapitalize="none"
-            autoCorrect={false}
-            icon="lock-outline"
-            placeholder="Password"
-            onChangeText={text => setPassword(text)}
-            secureTextEntry
-            textContentType="password"
-            />
-        <AppButton title="LOG IN" onPress={() => console.log(email, password)} />
+            <Image
+                style={styles.logo}
+                source={require("../assets/trolly.png")} />
+            <Formik
+                initialValues={{ email: '', password: '' }}
+                onSubmit={values => console.log(values)} >
+
+                {({ handleChange, handleSubmit }) => (
+                    <>
+                        <AppTextInput
+                            autoCapitalize="none"
+                            autoCorrect={false}
+                            icon="email-outline"
+                            keyboardType="email-address"
+                            onChangeText={handleChange("email")}
+                            placeholder="Email"
+                            textContentType="emailAddress" // iOS fill from keychain
+                        />
+                        <AppTextInput
+                            autoCapitalize="none"
+                            autoCorrect={false}
+                            icon="lock-outline"
+                            placeholder="Password"
+                            onChangeText={handleChange("password")}
+                            secureTextEntry
+                            textContentType="password"
+                        />
+                        <AppButton title="LOG IN" onPress={handleSubmit} />
+                    </>
+                )}
+
+            </Formik>
         </Screen>
     );
 }
