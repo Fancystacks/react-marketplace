3d7ee8c85e9e404f147642e39bbe8cd8e83c3b4a
Leah Migacz <lmigacz@msudenver.edu>
2020-07-06 13:58:05 -0600

    Add Images 🛒

--- App.js
+++ App.js
@@ -1,37 +1,25 @@
 import React, { useState } from 'react';
-import * as ImagePicker from 'expo-image-picker';
-import * as Permissions from 'expo-permissions'
 import Screen from './app/components/Screen';
-import { Button, Image } from 'react-native';
-import ImageInput from './app/components/ImageInput';
+import ImageInputList from './app/components/ImageInputList';
 
 
 export default function App() {
-  const [imageUri, setImageUri] = useState();
-  const requestPermission = async () => {
-    const { granted } = await ImagePicker.requestCameraPermissionsAsync();
-     if (!granted)
-     alert('Permission is needed to access the photo library.');
+  const [imageUris, setImageUris] = useState([]);
+
+  const handleAdd = uri => {
+    setImageUris([...imageUris, uri]);
   }
 
-  useState(() => {
-    requestPermission();
-  }, [])
-
-  const selectImage = async () => {
-    try {
-      const result = await ImagePicker.launchImageLibraryAsync();
-      if (!result.cancelled)
-      setImageUri(result.uri)
-    } catch (error) {
-      console.log('Error uploading image');
-    }
+  const handleRemove = uri => {
+    setImageUris(imageUris.filter(imageUri => imageUri !== uri));
   }
 
   return (
-    <Screen><Button title="Select Image" onPress={selectImage}/>
-    <Image source={{ uri: imageUri }} style={{ width: 200, height: 200 }}/>
-    <ImageInput imageUri={imageUri} />
+    <Screen>
+    <ImageInputList
+     imageUris={imageUris}
+     onAddImage={handleAdd} 
+     onRemoveImage={handleRemove}/>
     </Screen>
     );
   }
