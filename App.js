65afaa2f1cb06320475791c71c64811bd5a99045
Leah Migacz <lmigacz@msudenver.edu>
2020-07-17 16:39:57 -0600

    Auth Refresh Refactor 🛒

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
-import React, { useState, useEffect } from 'react';
-import { NavigationContainer, useNavigation } from '@react-navigation/native';
+import React, { useState } from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { AppLoading } from 'expo';
 
 import navigationTheme from './app/navigation/navigationTheme';
 import AppNavigator from './app/navigation/AppNavigator';
@@ -7,20 +8,18 @@ import OfflineNotice from './app/components/OfflineNotice';
 import AuthNavigator from './app/navigation/AuthNavigator';
 import AuthContext from './app/auth/context';
 import authStorage from './app/auth/storage';
-import JwtDecode from 'jwt-decode';
 
 export default function App() {
   const [user, setUser] = useState();
+  const [isReady, setIsReady] = useState(false);
 
-  const restoreToken = async () => {
-    const token = await authStorage.getToken();
-    if (!token) return;
-    setUser(JwtDecode(token));
+  const restoreUser = async () => {
+    const user = await authStorage.getUser();
+    if (user) setUser(user);
   }
 
-  useEffect(() => {
-    restoreToken()
-  }, []);
+  if (!isReady) 
+    return ( <AppLoading startAsync={restoreUser} onFinish={() => setIsReady(true)} /> )
 
   return (
     <AuthContext.Provider value={{ user, setUser }}>
