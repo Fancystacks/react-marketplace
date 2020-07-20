fe924066c3ce7773ee139d8f0f18601a29e1b9eb
Leah Migacz <lmigacz@msudenver.edu>
2020-07-19 23:17:44 -0600

    Environmental MGMT 🛒

--- app/utility/cache.js
+++ app/utility/cache.js
@@ -1,5 +1,5 @@
 import { AsyncStorage } from 'react-native';
-import moment from 'moment';
+import dayjs from 'dayjs';
 
 const expirationMinutes = 10;
 const prefix = 'cache';
@@ -17,9 +17,9 @@ const store = async (key, value) => {
 }
 
 const isExpired = (item) => {
-    const now = moment(Date.now());
-    const storedTime = moment(item.timestamp);
-    return now.diff(storedTime, 'minutes') > 10;
+    const now = dayjs();
+    const storedTime = dayjs(item.timestamp);
+    return now.diff(storedTime, 'minute') > 10;
        
 }
 
